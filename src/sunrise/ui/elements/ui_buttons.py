5ad8347362d8cb2994c5ab27b7ac5e779cec941b
not-louis-239 <243234869+not-louis-239@users.noreply.github.com>
2026-07-01 21:42:40 +0800

    Correct ui_buttons module to use ThemeKeys instead of removed DrawContext

--- src/sunrise/ui/elements/ui_buttons.py
+++ src/sunrise/ui/elements/ui_buttons.py
@@ -25,7 +25,7 @@ from pathlib import Path
 import pygame as pg
 from pygame.font import Font
 
-from sunrise.ui.elements.widget import DrawContext
+from sunrise.ui.themes import Theme, ThemeKey
 
 from ._img_container import ImageContainer
 from .widget import Widget
@@ -33,14 +33,29 @@ from .widget import Widget
 
 class _UIButton(Widget):
     def __init__(
-            self, *, flex: int = 0, text: str, font: pg.font.Font, inset: int = 0,
-            fixed_size: tuple[int, int] | None = None, icon_path: Path | None = None
+            self, *,
+            flex: int = 0, text: str = "", font: pg.font.Font, inset: int = 0,
+            fixed_size: tuple[int, int] | None = None, icon_path: Path | None = None,
+            k_fg: ThemeKey = ThemeKey.FG, k_fg_active: ThemeKey = ThemeKey.FG_ACTIVE,
+            k_bg: ThemeKey = ThemeKey.BG, k_bg_active: ThemeKey = ThemeKey.BG_ACTIVE,
+            k_border: ThemeKey = ThemeKey.BORDER, border_w: int = 2
         ) -> None:
+        # The k_* prefix identifies ThemeKeys.
+        # Hence, they are not to be used directly in place of colours,
+        # otherwise Python will throw up.
+
         super().__init__(flex=flex)
         self.text = text
         self.font = font
         self.inset = inset
         self.fixed_size = fixed_size
+
+        self.k_fg = k_fg
+        self.k_bg = k_bg
+        self.k_fg_active = k_fg_active
+        self.k_bg_active = k_bg_active
+        self.k_border = k_border
+        self.border_w = border_w
 
         self.img_container: ImageContainer | None = (
             ImageContainer(img_path=icon_path, start_size=self._preferred_icon_size())
@@ -59,12 +74,6 @@ class _UIButton(Widget):
         raise NotImplementedError
 
 class RectangularUIButton(_UIButton):
-    def __init__(
-            self, *, text: str, font: Font, inset: int,
-            fixed_size: tuple[int, int] | None = None, icon_path: Path | None = None
-        ) -> None:
-        super().__init__(text=text, font=font, inset=inset, fixed_size=fixed_size, icon_path=icon_path)
-
     def check_click(self, mouse_pos: tuple[int, int]) -> bool:
         return self.rect.collidepoint(mouse_pos)
 
@@ -74,31 +83,38 @@ class RectangularUIButton(_UIButton):
     def layout(self, rect: pg.Rect) -> None:
         self.rect = rect
 
-    def draw(self, surface: pg.Surface, ctx: DrawContext) -> None:
-        fg = ctx.fg if self.active else ctx.disabled_fg
+    def draw(self, surface: pg.Surface, current_theme: Theme) -> None:
+        k_fg = self.k_fg_active if self.active else self.k_fg
+        k_bg = self.k_bg_active if self.active else self.k_bg
 
         # Draw the button's background
-        bg_colour = ctx.active_bg if self.active else ctx.bg
-        pg.draw.rect(surface, bg_colour, self.rect)
+        pg.draw.rect(surface, current_theme[k_bg], self.rect)
 
         # Draw icon
         if self.img_container is not None:
-            img_surf = self.img_container.get_tinted_scaled_img(fg[:3], self.preferred_size())
+            img_surf = self.img_container.get_tinted_scaled_img(current_theme[k_fg], self.preferred_size())
             surface.blit(img_surf, img_surf.get_rect(center=self.rect.center))
 
         # Draw border
-        pg.draw.rect(surface, ctx.border, self.rect, width=ctx.border_w)
+        pg.draw.rect(surface, current_theme[self.k_border], self.rect, width=self.border_w)
 
         # Draw text
-        text_surface = ctx.font.render(self.text, True, fg)
+        text_surface = self.font.render(self.text, True, k_fg)
         surface.blit(text_surface, text_surface.get_rect(center=self.rect.center))
 
 class CircularUIButton(_UIButton):
     def __init__(
-            self, *, r: int = 0, flex: int = 0, text: str, font: Font, inset: int = 0,
-            fixed_size: tuple[int, int] | None = None, img_path: Path | None = None
+            self, *,
+            r: int = 0, flex: int = 0, text: str = "", font: Font, inset: int = 0,
+            fixed_size: tuple[int, int] | None = None, icon_path: Path | None = None,
+            k_fg: ThemeKey = ThemeKey.FG, k_fg_active: ThemeKey = ThemeKey.FG_ACTIVE,
+            k_bg: ThemeKey = ThemeKey.BG, k_bg_active: ThemeKey = ThemeKey.BG_ACTIVE,
+            k_border: ThemeKey = ThemeKey.BORDER, border_w: int = 2
         ) -> None:
-        super().__init__(flex=flex, text=text, font=font, inset=inset, fixed_size=fixed_size, icon_path=img_path)
+        super().__init__(
+            flex=flex, text=text, font=font, inset=inset, fixed_size=fixed_size, icon_path=icon_path,
+            k_fg=k_fg, k_fg_active=k_fg_active, k_bg=k_bg, k_bg_active=k_bg_active, k_border=k_border, border_w=border_w
+        )
         self.r = r
 
     def check_click(self, mouse_pos: tuple[int, int]) -> bool:
@@ -110,21 +126,22 @@ class CircularUIButton(_UIButton):
     def layout(self, rect: pg.Rect) -> None:
         self.rect = rect
 
-    def draw(self, surface: pg.Surface, ctx: DrawContext) -> None:
-        fg = ctx.fg if self.active else ctx.disabled_fg
+    def draw(self, surface: pg.Surface, current_theme: Theme) -> None:
+        k_fg = self.k_fg_active if self.active else self.k_fg
+        k_bg = self.k_bg_active if self.active else self.k_bg
 
         # Draw background
-        pg.draw.circle(surface, ctx.active_bg if self.active else ctx.bg, self.rect.center, self.r)
+        pg.draw.circle(surface, current_theme[k_bg], self.rect.center, self.r)
 
         # Draw icon, but bound to the button circle
         if self.img_container is not None:
-            img_surf = self.img_container.get_tinted_scaled_img(fg[:3], self.preferred_size())
+            img_surf = self.img_container.get_tinted_scaled_img(current_theme[k_fg], self.preferred_size())
             surface.blit(img_surf, img_surf.get_rect(center=self.rect.center))
 
         # Draw the text
-        text_surf = self.font.render(self.text, True, fg)
+        text_surf = self.font.render(self.text, True, current_theme[k_fg])
         surface.blit(surface, text_surf.get_rect(center=self.rect.center))
 
         # Draw the border if applicable
-        if ctx.border_w > 0:
-            pg.draw.circle(surface, ctx.border, self.rect.center, self.r, width=ctx.border_w)
+        if self.border_w > 0:
+            pg.draw.circle(surface, current_theme[self.k_border], self.rect.center, self.r, width=self.border_w)
