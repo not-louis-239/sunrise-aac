4de2e0cf1ffbcded08e08688d05c5a85dd24dc14
not-louis-239 <243234869+not-louis-239@users.noreply.github.com>
2026-07-06 17:32:20 +0800

    Improve TalkState with dynamic font scaling for buttons

--- src/sunrise/ui/states/talk.py
+++ src/sunrise/ui/states/talk.py
@@ -194,10 +194,22 @@ class _Renderer:
             text_y = rect.top
 
         # Draw the button label
+        if img is None:
+            if len(button.label) <= 2:
+                font = self.aac_inst.assets.fonts.talk_button_font_xl
+            elif len(button.label) <= 4:
+                font = self.aac_inst.assets.fonts.talk_button_font_l
+            elif len(button.label) <= 7:
+                font = self.aac_inst.assets.fonts.talk_button_font_m
+            else:
+                font = self.aac_inst.assets.fonts.talk_button_font
+        else:
+            font = self.aac_inst.assets.fonts.talk_button_font
+
         draw_text(
             surface=screen, pos=(text_centre_x, text_y),
             horiz_align="centre", vert_align="top" if img else "centre", colour=theme[ThemeKey.FG],
-            text=str(button.label), font_family=self.aac_inst.assets.fonts.talk_button_font
+            text=str(button.label), font_family=font
         )
 
     def draw_sentence_bar(self, screen: pg.Surface, in_moving_state: bool, is_selecting_coords: bool) -> None:
